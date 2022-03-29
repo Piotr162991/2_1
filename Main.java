fb7245162e5db6b10cf4940a578c77b4c33119c3
Piotr162991 <100698056+Piotr162991@users.noreply.github.com>
2022-03-29 12:24:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a=5;
+      for(int x = 1; x <= a; x++)
+        {
+          for(int y = 0; y < x; y++)
+          {
+            System.out.print("*");
+          }
+    System.out.print("\n");
+          }
+
+        }
+}
